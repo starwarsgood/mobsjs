9d06a00bccc729e6be044ba811af793a9c5d2b98
starwarsgood <comp822012016@mail.ru>
2016-09-15 17:37:51 +0300

    ggg

---
+++ zadanye two.js
@@ -0,0 +1,30 @@
+var one ={
+a:"5",
+b:"4",
+c:"3",
+v:"2",
+n:"1"
+}
+console.log(one);
+//////////////////
+var o = new Object()
+	o.POP1="one",
+	o.POP2="two",
+	o.POP3="three",
+	o.POP4="foure",
+	o.POP5="five"
+console.log(o);
+/////////////////
+var as = Object.create({inherited: 1}, {
+  foo: {
+    get: (function () { 
+      var closured = 'foo';
+      return function () {
+        return closured+'bar';
+      };
+    })()
+  }
+});
+
+as.foo;
+console.log(as.foo);
